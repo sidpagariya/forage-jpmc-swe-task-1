b0cda273868afc163b75f93645c61fba5b62e888
Sid Pagariya <sid@sidp.me>
2023-06-22 17:17:32 -0400

    Update and add more unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    assert getDataPoint(quotes[0]) == ('ABC', 120.48, 121.2, 120.84)
+    assert getDataPoint(quotes[1]) == ('DEF', 117.87, 121.68, 119.775)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    assert getDataPoint(quotes[0]) == ('ABC', 120.48, 119.2, 119.84)
+    assert getDataPoint(quotes[1]) == ('DEF', 117.87, 121.68, 119.775)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    assert getRatio(100, 50) == 2
+    assert getRatio(50, 100) == 0.5
+    assert getRatio(0, 100) == 0
+    assert getRatio(100, 0) == None
 
 
 
